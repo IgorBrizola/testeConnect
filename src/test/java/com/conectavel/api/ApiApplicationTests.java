6c1b2f8dd6c3f57a192fa1c78621b57e36a4da7c
Bieelogrom <gabrielamorim15436@gmail.com>
2024-06-17 19:54:24 -0300

    1.0

---
+++ src/test/java/com/conectavel/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectavel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
